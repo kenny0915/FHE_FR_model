3880e4d15fb8519bd6c171a6a46602bbb4b40e02
kenny0915 <910915kenny@gmail.com>
2026-08-24 13:27:04 +0800

    add run statistics script

---
+++ run_statistics.sh
@@ -0,0 +1,8 @@
+python eval/layer_statistics.py \
+  --config configs/ms1mv3_r50.py \
+  --checkpoint work_dirs/ms1mv3_r50/model.pt \
+  --ijb-root ijb/IJBC \
+  --target IJBC \
+  --num-images 1000 \
+  --batch-size 64 \
+  --output work_dirs/ms1mv3_r50/ijbc_layer_statistics
